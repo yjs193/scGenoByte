78ced3180d7c62c11d3ed1480c7f8caaeae92eb6
csyjs <97960328+yjs193@users.noreply.github.com>
2025-12-29 01:03:12 +0800

    Add files via upload

--- models_finetune.py
+++ models_finetune.py
@@ -1,14 +1,3 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
-
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
-# --------------------------------------------------------
-# References:
-# timm: https://github.com/rwightman/pytorch-image-models/tree/master/timm
-# DeiT: https://github.com/facebookresearch/deit
-# --------------------------------------------------------
-
 from functools import partial
 
 import torch
@@ -25,8 +14,7 @@ class VisionTransformer(timm.models.vision_transformer.VisionTransformer):
     """
         Vision Transformer with support for global average pooling
     """
-
-    def __init__(self, num_pathways=0, **kwargs):  # <-- 【修改点 1】: 添加 num_pathways 参数
+    def __init__(self, num_pathways=0, **kwargs):
         super(VisionTransformer, self).__init__(**kwargs)
         img_size = kwargs['img_size']
         patch_size = kwargs['patch_size']
@@ -34,17 +22,12 @@ class VisionTransformer(timm.models.vision_transformer.VisionTransformer):
         self.patch_embed = PatchEmbed(img_size, patch_size, embed_dim)
         self.pos_embed = nn.Parameter(torch.randn(1, self.patch_embed.num_patches + 1, embed_dim) * .02)
         self.blocks = nn.ModuleList([
-            FlashAttentionBlock(embed_dim, 8, 4, qkv_bias=True, attn_drop=0.1, norm_layer=nn.LayerNorm)
+            Block(embed_dim, 8, 4, qkv_bias=True, attn_drop=0.1, norm_layer=nn.LayerNorm)
             for i in range(12)])
 
-        # 【修改点 2】: 添加新的通路重构头
-        # self.head 已经由父类 __init__ 定义 (用于分类)
-        # 我们添加一个新的头用于通路重构
-        # 假设 self.embed_dim 是可用的 (它在父类中被设置)
         if num_pathways > 0:
             self.pathway_head = nn.Linear(self.embed_dim, num_pathways)
         else:
-            # 如果 num_pathways 为 0，则退化为原始模型
             self.pathway_head = nn.Identity()
 
         self.num_pathways = num_pathways
@@ -65,13 +48,8 @@ class VisionTransformer(timm.models.vision_transformer.VisionTransformer):
         return x[:, 0]
 
     def forward(self, x):
-        # 【修改点 3】: 修改 forward 方法以返回两个输出
         x_embed = self.forward_features(x)  # (B, embed_dim)
-
-        # 任务 1: 细胞分类
         x_class = self.head(x_embed)
-
-        # 任务 2: 通路重构
         if self.num_pathways > 0:
             x_pathway = self.pathway_head(x_embed)
         else:
